2180fc4c5a85c2c3c58b75531c70d6cde2446f28
RekyThareq <rekythareq@gmail.com>
2024-04-16 12:33:03 +0700

    change title logo
    Rubah logo di bagian title

--- contact-us.php
+++ contact-us.php
@@ -54,7 +54,7 @@ if($lastInsertId){
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
 <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link rel="shortcut icon" href="assets/images/favicon-icon/logo3.png">
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
  <style>
     .errorWrap {
--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3 col-md-2">
-          <div class="logo"> <a href="index.php"><img style="width: 150px" src="assets/images/logo-ozan.png" alt="image"/></a> </div>
+          <div class="logo"> <a href="index.php"><img style="width: 150px" src="assets/images/logo99.png" alt="image"/></a> </div>
         </div>
         <div class="col-sm-9 col-md-10">
           <div class="header_info">
